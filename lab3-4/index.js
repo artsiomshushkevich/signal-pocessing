c60a0934f5dc77bf552dcb71b6cf63e73a94b1fa
Artsiom Shushkevich <artemshushkevich@live.com>
2016-12-05 01:37:41 +0300

    add visualization

--- lab3-4/index.js
+++ lab3-4/index.js
@@ -2,7 +2,10 @@
     let fileReader = new FileReader();
     let context = new AudioContext();
     let gainNode = context.createGain();
-    let source, balanceFilters, frequencyFilter;
+    let canvasWidth = $('#visualizer').width();
+    let canvasHeight = $('#visualizer').height();
+
+    let source, balanceFilters, frequencyFilter, analyser, requestId;
 
     fileReader.onload = (event) => {
         let buffer = event.srcElement.result;
@@ -15,17 +18,51 @@
             balanceFilters = createBalanceFilters();
             frequencyFilter = createFrequencyFilter();
 
+            analyser = context.createAnalyser();
+            analyser.fftSize = 4096;
+            
             source.connect(balanceFilters[0]);
             balanceFilters[balanceFilters.length - 1].connect(frequencyFilter);
             frequencyFilter.connect(gainNode); 
-            gainNode.connect(context.destination);
-
+            gainNode.connect(analyser);
+            analyser.connect(context.destination);
             source.start(0);   
+
+            requestId = requestAnimationFrame(draw);
+         
         });             
     };
 
-    $('#lol').on('change', (event) => {
-        filter.gain.value = +event.target.value;
+    function draw() {
+        let canvasContext = $('#visualizer')[0].getContext('2d');
+        canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+
+        let frequencies = new Uint8Array(analyser.frequencyBinCount);
+        let times = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(frequencies);
+        analyser.getByteTimeDomainData(times);
+
+        for (let i = 0; i < analyser.frequencyBinCount; i++) {
+            var height = (frequencies[i] / 256) * canvasHeight;
+            var offset = canvasHeight - height;
+            var barWidth = canvasWidth/analyser.frequencyBinCount;
+            canvasContext.fillStyle = '#2c3e50';
+            canvasContext.fillRect(i * barWidth, offset, barWidth, height);
+        }
+
+        for (let i = 0; i < analyser.frequencyBinCount; i++) {
+            var height =  (times[i] / 256) * canvasHeight;
+            var offset = canvasHeight - height - 1;
+            var barWidth = canvasWidth / analyser.frequencyBinCount;
+            canvasContext.fillStyle = 'red';
+            canvasContext.fillRect(i * barWidth, offset, 1, 2);
+        }
+
+        requestId = requestAnimationFrame(draw);
+    }
+
+    $('#smoothing').on('change', (event) => {
+        analyser.smoothingTimeConstant = +event.target.value;
     });
 
     $('#play-but').on('click', () => {  
@@ -39,7 +76,10 @@
         fileReader.readAsArrayBuffer(file); 
     });
     
-    $('#stop-but').on('click', () => source.stop());
+    $('#stop-but').on('click', () => {
+        source.stop();
+        cancelAnimationFrame(requestId);
+    });
 
     $('#volume-range').on('change', (event) => {
         gainNode.gain.value = event.target.value / event.target.max;
